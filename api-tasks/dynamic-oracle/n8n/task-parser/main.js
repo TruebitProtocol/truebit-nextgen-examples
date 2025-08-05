346066dc66edd247c9dfec68c06346a956c104d1
senatorec <clara.senatore@fortegrp.com>
2025-08-05 15:18:49 -0300

    feat: add comment

--- api-tasks/dynamic-oracle/n8n/task-parser/main.js
+++ api-tasks/dynamic-oracle/n8n/task-parser/main.js
@@ -3,8 +3,8 @@ import { writeFileSync, readFileSync } from 'fs';
 function create(res) {
   
   const body = JSON.parse(res);
-  const values = [body[0].id, body[0].name];
-  const types = ['uint256', 'string'];
+  const values = [body[0].id, body[0].name]; // Replace with the fields you want to encode
+  const types = ['uint256', 'string']; // Match the Solidity types of the values above (same order)
   const encodedInput = defaultAbiCoder.encode(types, values);
   return encodedInput.toString();
 }
